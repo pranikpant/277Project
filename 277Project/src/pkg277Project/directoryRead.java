3353358e77bc4bead7e3cbc2b5df6efd089675ad
Pranik Pant <pranikpant@gmail.com>
2021-05-03 16:35:32 -0700

    Create directoryRead.java
    added a new class file that reads and prints out all the files/directories along with their size and date last modified at a specified location

---
+++ 277Project/src/pkg277Project/directoryRead.java
@@ -0,0 +1,24 @@
+package pkg277Project;
+
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.text.DecimalFormat;
+
+public class directoryRead {
+    
+    public static void main(String[] args) {
+        File file = new File("C:\\Users\\Pranik\\Downloads\\CECS341_Lab5_Pant_Lam");
+        File[] files;
+        files = file.listFiles();
+
+        for (int i = 0; i < files.length; i++) {
+            SimpleDateFormat date = new SimpleDateFormat("MM/dd/yyyy");
+            DecimalFormat size = new DecimalFormat("#,###");
+            if (files[i].isDirectory()) {
+                System.out.println("Directory:" + files[i].getAbsolutePath() + " Date:" + date.format(files[i].lastModified()) + " Size:" + size.format(files[i].length()));
+            } else {
+                System.out.println("File:" + files[i].getAbsolutePath() + " Date:" + date.format(files[i].lastModified()) + " Size:" + size.format(files[i].length()));
+            }
+        }
+    }
+}   
