117493d7848f6b13bf13ea0c2374492557eaac99
Pranik Pant <pranikpant@gmail.com>
2021-05-03 16:37:58 -0700

    reads directory

--- 277Project/src/pkg277Project/directoryRead.java
+++ 277Project/src/pkg277Project/directoryRead.java
@@ -7,7 +7,7 @@ import java.text.DecimalFormat;
 public class directoryRead {
     
     public static void main(String[] args) {
-        File file = new File("C:\\Users\\Pranik\\Downloads\\CECS341_Lab5_Pant_Lam");
+        File file = new File("C:\\");
         File[] files;
         files = file.listFiles();
 
@@ -15,9 +15,9 @@ public class directoryRead {
             SimpleDateFormat date = new SimpleDateFormat("MM/dd/yyyy");
             DecimalFormat size = new DecimalFormat("#,###");
             if (files[i].isDirectory()) {
-                System.out.println("Directory:" + files[i].getAbsolutePath() + " Date:" + date.format(files[i].lastModified()) + " Size:" + size.format(files[i].length()));
+                System.out.println("Directory:" + files[i].getAbsolutePath() + " Date: " + date.format(files[i].lastModified()) + " Size: " + size.format(files[i].length()));
             } else {
-                System.out.println("File:" + files[i].getAbsolutePath() + " Date:" + date.format(files[i].lastModified()) + " Size:" + size.format(files[i].length()));
+                System.out.println("File:" + files[i].getAbsolutePath() + " Date: " + date.format(files[i].lastModified()) + " Size: " + size.format(files[i].length()));
             }
         }
     }
