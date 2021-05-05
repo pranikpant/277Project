bd05e1ffd317ff016b4bf604a6728a82ab79aa5c
Pranik Pant <pranikpant@gmail.com>
2021-05-04 20:31:28 -0700

    drives names

--- 277Project/src/pkg277Project/pkg277Project/getDrives.java
+++ 277Project/src/pkg277Project/pkg277Project/getDrives.java
@@ -4,13 +4,13 @@ import java.util.ArrayList;
 
 public class getDrives {
     
-    public ArrayList<File> allDrives(){
-        ArrayList<File> drives = new ArrayList<File>();
+    public ArrayList<String> allDrives(){
+        ArrayList<String> drives = new ArrayList<String>();
         File[] paths;
         // number of dropdown items (items 1-4 etc.) = paths.length
         paths = File.listRoots();
         for(int i = 0; i < paths.length; i++){
-            drives.add(paths[i]);
+            drives.add(paths[i].getName());
         }
         return drives;
     }   
